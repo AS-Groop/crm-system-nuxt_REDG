9b3bfd86cf415da26426ec612b18af88ba4b1720
AS-Groop <asadbek.sheraliyev07@mail.ru>
2024-11-25 14:05:34 +0500

    begin kanban

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {ICard, IColumn} from "~/components/kanban/kanban.types";
+
+useSeoMeta({
+  title: "Home | CRM system"
+})
+
+const dragCard = ref<ICard | null>(null)
+const sourceColumn = ref<IColumn | null>(null)
+
+
+</script>
 
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System by AS-GROOP</h1>
+    <UiCard class="mb-3" draggable="true">
+      <UiCardHeader role="button"> name card</UiCardHeader>
+      <UiCardContent>Company</UiCardContent>
+      <UiCardFooter>Date</UiCardFooter>
+    </UiCard>
   </div>
 </template>
